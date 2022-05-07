648f80adfd973f8ea799bdb9440bbe28d65a1da8
amir <amirsazegar16@gmail.com>
2022-05-07 22:59:47 +0330

    created actions for articles and categories in admin panel.

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,42 @@
 from django.contrib import admin
 
 from .models import Article, Category
-# Register your models here.
+
+def article_published(modeladmin, request, queryset):
+    rows_updated = queryset.update(status='p')
+    if rows_updated == 1:
+        message_bit = "منتشر شد."
+    else:
+        message_bit = "منتشر شدند."
+    modeladmin.message_user(request, "{} مقاله {}".format(rows_updated, message_bit))
+article_published.short_description = 'انتشار مقالات انتخاب شده'
+
+def article_draft(modeladmin, request, queryset):
+    rows_updated = queryset.update(status='d')
+    if rows_updated == 1:
+        message_bit = "پیش نویس شد."
+    else:
+        message_bit = "پیش نویس شدند."
+    modeladmin.message_user(request, "{} مقاله {}".format(rows_updated, message_bit))
+article_draft.short_description = 'پیش نویس مقالات انتخاب شده'
+
+def category_published(modeladmin, request, queryset):
+    rows_updated = queryset.update(status=True)
+    if rows_updated == 1:
+        message_bit = "منتشر شد."
+    else:
+        message_bit = "منتشر شدند."
+    modeladmin.message_user(request, "{} دسته بندی {}".format(rows_updated, message_bit))
+category_published.short_description = 'انتشار دسته بندی های انتخاب شده'
+
+def category_draft(modeladmin, request, queryset):
+    rows_updated = queryset.update(status=False)
+    if rows_updated == 1:
+        message_bit = "پیش نویس شد."
+    else:
+        message_bit = "پیش نویس شدند."
+    modeladmin.message_user(request, "{} دسته بندی {}".format(rows_updated, message_bit))
+category_draft.short_description = 'پیش نویس دسته بندی های انتخاب شده'
 
 
 class ArticleAdmin(admin.ModelAdmin):
@@ -11,11 +46,11 @@ class ArticleAdmin(admin.ModelAdmin):
     search_fields = ('title', 'description')
     prepopulated_fields = {'slug': ('title',)}
     ordering = ('-status', '-publish')
+    actions = [article_published, article_draft]
 
     def category_to_str(self, obj):
         return "، ".join([category.title for category in obj.category_published()])
     category_to_str.short_description = "دسته بندی"
-
 
 admin.site.register(Article, ArticleAdmin)
 
@@ -26,6 +61,7 @@ class CategoryAdmin(admin.ModelAdmin):
     list_filter = ('status',)
     search_fields = ('title', 'slug')
     prepopulated_fields = {'slug': ('title',)}
+    actions = [category_published, category_draft]
 
 
 admin.site.register(Category, CategoryAdmin)
